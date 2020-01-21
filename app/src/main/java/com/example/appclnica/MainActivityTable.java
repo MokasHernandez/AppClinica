b50d595da2fbbab02f16f4ceb6e7e7a55926d572
Mokas Hernández <59838679+MokasHernandez@users.noreply.github.com>
2020-01-21 10:06:00 -0600

    Cambio 21/01/2020

---
+++ app/src/main/java/com/example/appclnica/MainActivityTable.java
@@ -0,0 +1,61 @@
+package com.example.appclnica;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+
+public class MainActivityTable extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    PageAdapter pageAdapter;
+    TabItem tabChats;
+    TabItem tabStatus;
+    TabItem tabCalls;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_table);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tabLayout = findViewById(R.id.tablayout);
+        tabChats = findViewById(R.id.tabChats);
+        tabStatus = findViewById(R.id.tabStatus);
+        tabCalls = findViewById(R.id.tabCalls);
+        viewPager = findViewById(R.id.viewPager);
+
+        pageAdapter = new PageAdapter(this, getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+}
+
---
+++ app/src/main/java/com/example/appclnica/PageAdapter.java
@@ -0,0 +1,38 @@
+package com.example.appclnica;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    private int numOfTabs;
+    private final Context myContext;
+
+    public PageAdapter(Context context, FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numOfTabs = numOfTabs;
+        myContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new ChatFragment();
+            case 1:
+                return new StatusFragment();
+            case 2:
+                return new CallFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+}
