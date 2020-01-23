dbee7ce62659e5df1db9756dfde449070656d484
brandon1968 <brandon_floressan@hotmail.com>
2020-01-22 23:52:55 -0600

    añadi la parte de Mantenimientos

--- app/src/main/java/com/example/appclnica/Mantenimientos.java
+++ app/src/main/java/com/example/appclnica/Mantenimientos.java
@@ -2,13 +2,31 @@ package com.example.appclnica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class Mantenimientos extends AppCompatActivity {
+
+    private CheckBox cb1,cb2;
+    Button button;
+    ArrayList nmb;
+    EditText EDTEMPRESA,EDTFECHA,EDTHPARO,EDTEQUIPO,EDTCOSTO,EDTDESF,EDTREFS,EDTTIPO;
+    private int InYear,Inmonth,InDay,AyearIn,AmonthIn,AdayIn;
+    DatePickerDialog.OnDateSetListener setListener;
+    String B;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_mantenimientos );
+        Intent I = new Intent( Mantenimientos.this, StatusFragment.class );
+
     }
 }
