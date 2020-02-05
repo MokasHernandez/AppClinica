4468605f5a9e5c5703f5ab27d3c252ce29c26e2c
Mokas Hernández <59838679+MokasHernandez@users.noreply.github.com>
2020-02-05 16:18:19 -0600

    Cambio 05/02/2020

--- app/src/main/java/com/example/appclnica/ActivityAlmacen.java
+++ app/src/main/java/com/example/appclnica/ActivityAlmacen.java
@@ -8,8 +8,12 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.hardware.camera2.TotalCaptureResult;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
@@ -58,12 +62,13 @@ public class ActivityAlmacen extends AppCompatActivity implements AdapterView.On
     private TextView lbProvee, lbOrden, lbFactura, lbIVA;
     private EditText txtFactura, txtIVA;
     Button btnOrdenC, btnAgregarT;
-    Spinner SProveedor, SPedido, SOrden, STatus;
+    Spinner SProveedor, SPedido, SOrden;
     ArrayList<String> folioPedidos, proveedores, ordenesC, status;
     ArrayList<String[]> nombres;
     ArrayList<CheckBox> checkBoxes;
     TableLayout tablaProductos;
-    String orden, folioe;
+    String orden, folioe, num_ent;
+    Boolean bandera = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,7 +91,7 @@ public class ActivityAlmacen extends AppCompatActivity implements AdapterView.On
 
         setSupportActionBar(toolbar);
 
-        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN & WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN | WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
         status = new ArrayList<String>();
         status.add("Aprobado");
@@ -110,45 +115,31 @@ public class ActivityAlmacen extends AppCompatActivity implements AdapterView.On
                     TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
                     fila.setLayoutParams(lp);
 
-                    /*final CheckBox ch = new CheckBox(getApplicationContext());
-                    ch.setChecked(true);
-                    fila.addView(ch);*/
                     LlenarTextView("", fila, 1);
                     LlenarTextView("Nombre_Producto  ", fila, 1);
                     LlenarTextView("Cantidad Unidad  ", fila, 1);
                     LlenarTextView("Unidad Entrada  ", fila, 1);
                     LlenarTextView("Unidad Salida  ", fila, 1);
                     LlenarTextView("Factor entre unidades  ", fila, 1);
-                    LlenarTextView(" Descuento ", fila, 1);
-                    LlenarTextView("  ", fila, 1);
                     LlenarTextView("Costo Unitario  ", fila, 1);
                     LlenarTextView("Costo Total  ", fila, 1);
+                    LlenarTextView("  ", fila, 1);
+                    LlenarTextView(" Descuento ", fila, 1);
                     LlenarTextView(" ", fila, 1);
                     LlenarTextView("     Status", fila, 1);
                     tablaProductos.addView(fila, 0);
-
-                    /*ch.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                        for (int i = 0; i < checkBoxes.size(); i++){
-                                if (ch.isChecked()){
-                                    checkBoxes.get(i).setChecked(true);
-                                }else{
-                                    checkBoxes.get(i).setChecked(false);
-                                }
-                            }
-                        }
-                    });*/
 
                     nombres = new ArrayList<String[]>();
                     checkBoxes = new ArrayList<CheckBox>();
                     CargarDatosTabla("http://asesoresconsultoreslabs.com/asesores/App_Android/Almacen.php?id=3&folio=" + folioe);
 
-                    txtFactura.clearFocus();
                     lbIVA.setVisibility(View.VISIBLE);
                     txtIVA.setVisibility(View.VISIBLE);
                     tablaProductos.setVisibility(View.VISIBLE);
                     btnAgregarT.setVisibility(View.VISIBLE);
+                    btnOrdenC.setVisibility(View.INVISIBLE);
+                    txtFactura.setVisibility(View.INVISIBLE);
+                    lbFactura.setVisibility(View.INVISIBLE);
 
                 }else{
                     txtFactura.setError("Favor de llenar éste campo");
@@ -161,11 +152,25 @@ public class ActivityAlmacen extends AppCompatActivity implements AdapterView.On
             public void onClick(View v) {
                 for (int i = 0; i < nombres.size(); i++){
                     String[] a = nombres.get(i);
-                    String c = a[0];
-                    String d = a[1];
-                    String e = a[2];
-                    Toast.makeText(getApplicationContext(), c + " " + d + " " + e, Toast.LENGTH_SHORT).show();
+                    String materia  = a[0];
+                    String uni_ent = a[1];
+                    String fact_entre = a[2];
+                    String canti = a[3];
+                    String lote = a[4];
+                    String cadu = a[5];
+                    String costo = a[6];
+                    String desc = a[7];
+                    String cumple = a[8];
+                    InsertDetalle_Orden(materia, num_ent, uni_ent, fact_entre, canti, lote, cadu, costo, desc,
+                            txtIVA.getText().toString(), SPedido.getSelectedItem().toString(), cumple);
                 }
+                if (bandera == true){
+                    Toast.makeText(getApplicationContext(), "Se registraron los datos con éxito", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Error en los registros", Toast.LENGTH_LONG).show();
+                }
+                SProveedor.setSelection(0);
             }
         });
     }
@@ -215,6 +220,7 @@ public class ActivityAlmacen extends AppCompatActivity implements AdapterView.On
                 SOrden.setVisibility(View.VISIBLE);
 
             }else{
+                tablaProductos.removeAllViews();
                 lbOrden.setVisibility(View.INVISIBLE);
                 SOrden.setVisibility(View.INVISIBLE);
                 tablaProductos.setVisibility(View.INVISIBLE);
@@ -310,9 +316,8 @@ public class ActivityAlmacen extends AppCompatActivity implements AdapterView.On
                             TextView tv = new TextView(getApplicationContext());
                             final EditText Descuento = new EditText(getApplicationContext());
                             final Spinner Status = new Spinner(getApplicationContext());
-                            EditText Lote = new EditText(getApplicationContext());
+                            final EditText Lote = new EditText(getApplicationContext());
                             final EditText Cad = new EditText(getApplicationContext());
-                            STatus = Status;
 
                             select.setChecked(true);
                             fila.addView(select);
@@ -325,13 +330,12 @@ public class ActivityAlmacen extends AppCompatActivity implements AdapterView.On
                             LlenarTextView(uni_en, fila, 0);
                             LlenarTextView(uni_sa, fila, 0);
                             LlenarTextView(factor, fila, 0);
-
-                            FormarEdit(Descuento, fila, 0, "Des");
-                            LlenarTextView("  ", fila, 1);
-
                             LlenarTextView(costo_ind, fila, 0);
                             LlenarTextView(costo_tot, fila, 0);
-                            LlenarTextView(" ", fila, 1);
+
+                            LlenarTextView("  ", fila, 1);
+                            FormarEdit(Descuento, fila, 0, "Des");
+                            LlenarTextView("  ", fila, 1);
 
                             LlenarSpinners(status, Status);
                             Status.setBackground(getResources().getDrawable(R.drawable.txt));
@@ -353,19 +357,79 @@ public class ActivityAlmacen extends AppCompatActivity implements AdapterView.On
 
                             tablaProductos.addView(fila);
 
-                            final String[] a = new String[]{m, can, "SI"};
+                            final String[] a = new String[]{m, uni_en, factor, can, "", "", costo_ind, Descuento.getText().toString(),
+                                    "SI"};
                             nombres.add(a);
 
                             //checkBoxes.add(select);
+
+                            Descuento.addTextChangedListener(new TextWatcher() {
+                                @Override
+                                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                                }
+
+                                @Override
+                                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                                }
+
+                                @Override
+                                public void afterTextChanged(Editable s) {
+                                    a[7] = Descuento.getText().toString();
+                                }
+                            });
+
+                            Lote.addTextChangedListener(new TextWatcher() {
+                                @Override
+                                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                                }
+
+                                @Override
+                                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                                    a[4] = Lote.getText().toString();
+                                }
+
+                                @Override
+                                public void afterTextChanged(Editable s) {
+
+                                }
+                            });
+
+                            Cad.addTextChangedListener(new TextWatcher() {
+                                @Override
+                                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                                }
+
+                                @Override
+                                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                                }
+
+                                @Override
+                                public void afterTextChanged(Editable s) {
+                                    a[5] = Cad.getText().toString();
+                                }
+                            });
+
+                            Cad.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    showDatePickerDialog(Cad);
+                                }
+                            });
 
                             Status.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                                 @Override
                                 public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                                     if (position == 0){
-                                        a[2] = "SI";
+                                        a[8] = "SI";
 
                                     }else{
-                                        a[2] = "NO";
+                                        a[8] = "NO";
+                                        Descuento.requestFocus();
                                     }
                                 }
 
@@ -432,11 +496,13 @@ public class ActivityAlmacen extends AppCompatActivity implements AdapterView.On
             if (fondo == "  Caducidad  "){
                 editText.setHint(fondo);
                 editText.setHintTextColor(Color.GRAY);
+                editText.setFocusable(false);
+                editText.setInputType(InputType.TYPE_DATETIME_VARIATION_DATE);
             }
             else if(fondo == "Des"){
                 editText.setText("0");
+                editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
             }
-            editText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
         }else{
             editText.setInputType(InputType.TYPE_CLASS_TEXT);
             editText.setHint(fondo);
@@ -452,6 +518,7 @@ public class ActivityAlmacen extends AppCompatActivity implements AdapterView.On
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        num_ent = response;
                         Toast.makeText(getApplicationContext(), "Se registro exitosamente", Toast.LENGTH_SHORT).show();
                     }
                 }, new Response.ErrorListener() {
@@ -472,5 +539,65 @@ public class ActivityAlmacen extends AppCompatActivity implements AdapterView.On
         };
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(request);
+    }
+
+    private void InsertDetalle_Orden(String materia, final String no, final String entrada, final String factor, final String canti, final String lote
+    , final String caducidad, final String costo, final String desc, final String iva, final String pedido, final String cumple) {
+        materia = materia.replace(" ", "%20").trim();
+        StringRequest request = new StringRequest(Request.Method.POST,
+                "http://asesoresconsultoreslabs.com/asesores/App_Android/Almacen.php?id=5&nom_mat=" + materia,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        bandera = true;
+                        //Toast.makeText(getApplicationContext(), "Se registro con éxito", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                bandera = false;
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("no_entrada", no);
+                parametros.put("unidad_entrada", entrada);
+                parametros.put("factor_entre_unidades", factor);
+                parametros.put("cantidad", canti);
+                parametros.put("lote", lote);
+                parametros.put("caducidad", caducidad);
+                parametros.put("costo", costo);
+                parametros.put("descuento", desc);
+                parametros.put("iva", iva);
+                parametros.put("num_pedido", pedido);
+                parametros.put("cumple", cumple);
+                return parametros;
+            }
+        };
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
+
+    public void showDatePickerDialog(final EditText uso){
+        com.example.appclnica.DatePicker newFragment = com.example.appclnica.DatePicker.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(android.widget.DatePicker view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                String selectedDate = year + "-" + (month) + "-" + dayOfMonth;
+                if (month < 10){
+                    selectedDate = year + "-0" + (month) + "-" + dayOfMonth;
+                }
+                if (dayOfMonth < 10){
+                    selectedDate = year + "-" + (month) + "-0" + dayOfMonth;
+                }
+                if (month < 10 & dayOfMonth < 10) {
+                    selectedDate = year + "-0" + (month) + "-0" + dayOfMonth;
+                }
+                uso.setText(selectedDate);
+            }
+        });
+
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
---
+++ app/src/main/java/com/example/appclnica/DatePicker.java
@@ -0,0 +1,38 @@
+package com.example.appclnica;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+
+public class DatePicker extends DialogFragment{
+
+    private DatePickerDialog.OnDateSetListener listener;
+
+    public static DatePicker newInstance(DatePickerDialog.OnDateSetListener listener) {
+        DatePicker fragment = new DatePicker();
+        fragment.setListener(listener);
+        return fragment;
+    }
+
+    public void setListener(DatePickerDialog.OnDateSetListener listener) {
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), listener, year, month, day);
+    }
+}
